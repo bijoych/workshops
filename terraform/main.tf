5bd527fe024278ff565758ac665edbd294669def
Bijoy Choudhury <43165134+bijoych@users.noreply.github.com>
2025-07-03 17:22:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,37 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "kafka_key" {
-  key_name   = "kafka-key"
-  public_key = file("/Users/bijoychoudhury/.ssh/id_rsa.pub") # Path to your public SSH key
+# Generate a new private key using the TLS provider
+resource "tls_private_key" "rsa_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the key pair in AWS using the public key from the generated key
+resource "aws_key_pair" "generated_key" {
+  key_name   = "my-tf-generated-key"
+  public_key = tls_private_key.rsa_key.public_key_openssh
+}
+
+# Save the generated private key to a local file
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.rsa_key.private_key_pem
+  filename = "${path.module}/my-tf-key.pem"
+  file_permission = "0400" # Set correct permissions
 }
 
 resource "aws_security_group" "kafka_sg" {
@@ -41,12 +68,18 @@ resource "aws_security_group" "kafka_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance."
+  value       = aws_instance.kafka_ec2.public_ip
 }
 
 resource "aws_instance" "kafka_ec2" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+  ami                    = "ami-05ffe3c48a9991133" # Amazon Linux 2 AMI
   instance_type          = "t2.medium"
-  key_name               = aws_key_pair.kafka_key.key_name
+  key_name               = aws_key_pair.generated_key.key_name
   vpc_security_group_ids = [aws_security_group.kafka_sg.id]
 
   user_data = <<-EOF
@@ -73,9 +106,18 @@ resource "aws_instance" "kafka_ec2" {
 
               # Start Kafka
               nohup /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties > /tmp/kafka.log 2>&1 &
+
+              # Set the PATH in .bash_profile file
+              echo 'export PATH="$PATH:/opt/kafka/bin/"' >> /home/ec2-user/.bash_profile
+              source /home/ec2-user/.bash_profile
+
+              # Install Confluent CLI
+              curl -sL --http1.1 https://cnfl.io/cli | sh -s --
+              sudo mv ./bin/confluent /usr/local/bin/
+
               EOF
 
-  tags = {
-    Name = "KafkaEC2"
-  }
+    tags = {
+    Name = "Kafka_EC2"
+    }
 }
